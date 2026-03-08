49d0944c2121013830d8e05cc37586c2807a3f8c
junaidify <junaidkhan23785@gmail.com>
2026-03-08 15:57:00 +0530

    learning_pydantic

---
+++ __init__.py
@@ -0,0 +1,19 @@
+# app = JobApplication(
+#     name="  junaid khan  ",
+#     email="junaidkhan@gmail.com",
+#     experience_years=5,
+#     skills=["Python", "react", "KUBERNETES"],
+#     cover_letter="I want job. I am good.",
+#     github_url="https://github.com/junaidify"
+# )
+
+
+# {
+#   name: "Junaid Khan"          ← auto formatted
+#   email: "junaidkhan@gmail.com" ← validated
+#   experience_years: 5          ← must be 0-50
+#   skills: ["Python", "React", "Kubernetes"]  ← auto normalized
+#   cover_letter: "I want job..."  ← min 20 words enforced
+#   github_url: "https://github.com/junaidify" ← URL validated
+#   score: 72                    ← computed automatically
+# }
