f0c19a57c8b0f94ae075653b9923983870c05c4c
Junaid Khan <157748393+junaidify@users.noreply.github.com>
2026-03-16 23:25:42 +0530

    learning_nn

--- pytorch/test_nn.py
+++ pytorch/test_nn.py
@@ -1,2 +1,28 @@
-import pandas as pd
-import numpy as np 
+import torch
+from torch import nn
+
+device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else 'cpu'
+print(device)
+
+
+class NeuralNetwork(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(), 
+            nn.Linear(512, 512), 
+            nn.ReLU(), 
+            nn.Linear(512, 10)
+        )
+
+    
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+
+model = NeuralNetwork().to(device)
+print(model)
